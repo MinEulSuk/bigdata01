0e694cf797ed4f42fde3e2c8a201e5d98fd272d6
Min <202130208@email.daelim.ac.kr>
2025-03-20 11:45:04 +0900

    v0.3 prime number

--- week03_prime.py
+++ week03_prime.py
@@ -1,10 +1,13 @@
 number = int(input())
 count = 0
 
-for i in range(1,number+1):
-    if number % i == 0:
-        count += 1
-    print(i, end=" ")
+if count >= 2:
+    for i in range(2,number):
+        if number % i == 0:
+            count += 1
+        print(i, end=" ")
+else:
+    count = 1
 
 if count == 2:
     print(f'{number}는 소수입니다.')
