6821aeea4892fee40d98dd8d6f758f886e4dbd1a
Min <202130208@email.daelim.ac.kr>
2025-03-20 11:46:05 +0900

    v0.4 prime number

--- week03_prime.py
+++ week03_prime.py
@@ -1,15 +1,16 @@
 number = int(input())
-count = 0
+is_prime = True
 
-if count >= 2:
+if number >= 2:
     for i in range(2,number):
         if number % i == 0:
-            count += 1
+            is_prime = False
+            break
         print(i, end=" ")
 else:
-    count = 1
+    is_prime = False
 
-if count == 2:
+if is_prime:
     print(f'{number}는 소수입니다.')
 else:
     print(f'{number}는 소수가 아닙니다.')
