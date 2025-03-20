7a1b6b9541ea7c80251d801da4006f81dc615dfc
Min <202130208@email.daelim.ac.kr>
2025-03-20 11:51:49 +0900

    v0.5 prime number

--- week03_prime.py
+++ week03_prime.py
@@ -2,11 +2,14 @@ number = int(input())
 is_prime = True
 
 if number >= 2:
-    for i in range(2,number):
+    # for i in range(2,number):
+    i = 2
+    while i * i <= number:
         if number % i == 0:
             is_prime = False
             break
         print(i, end=" ")
+        i += 1
 else:
     is_prime = False
 
