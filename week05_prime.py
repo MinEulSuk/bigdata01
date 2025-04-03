5ad0e69d8c5a6df6c6b2cc1ed36b47c6580b9fb2
Min <202130208@email.daelim.ac.kr>
2025-04-03 12:23:28 +0900

    0.9prime number

---
+++ week05_prime.py
@@ -0,0 +1,36 @@
+def is_prime(number : int) -> bool:
+    #number : int -> bool은 권고사항이라 맞지 않아도 작동은 함
+    #같은 개발자에대한 권고사항정도 넣을땐 int 나올땐 bool
+    """
+    function to determine whether number is prime
+
+
+    :param number: 판정할 숫자
+    :return: True if it is prime number else False
+    """
+
+    if number >= 2:
+        for i in range(2,int(number**0.5)+1):
+            if number % i == 0:
+                return False
+            # print(i, end=" ")
+    else:
+        return False
+    return True
+
+n1 = int(input())
+n2 = int(input())
+
+
+for i in range(n1,n2+1):
+    if is_prime(i):
+        print(i,end=' ')
+
+
+# n = int(input())
+#
+#
+# if is_prime(n):
+#     print(f'{n}는 소수입니다.')
+# else:
+#     print(f'{n}는 소수가 아닙니다.')
