59e6a346848f925b9282bd2b7a60da36c59e9d62
Min <202130208@email.daelim.ac.kr>
2025-05-15 10:15:51 +0900

    3.0 kiosk run to kiosk.py

--- week10_kiosk.py
+++ week10_kiosk.py
@@ -4,18 +4,6 @@ import kiosk as kk
 
 
 if __name__ == "__main__":
-    while True:
-        try:
-            menu = int(input(kk.display_menu()))
-            if 1 <= menu <= len(kk.drinks):
-                kk.order_process(menu-1)
-            elif menu == len(kk.drinks)+1:
-                print("주문을 종료합니다.")
-                break
-            else:
-                print(f'{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요.')
-        except ValueError:
-            print(f"문자를 입력할 수 없습니다.. 숫자를 입력해주세요.")
-
+    kk.run()
     kk.print_receipt()
     print(f'번호표 : {kk.get_ticket_number()}')
