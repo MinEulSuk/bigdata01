d415b5f6bf1958cd9ae661384c295a150548f6b6
Min <202130208@email.daelim.ac.kr>
2025-05-29 10:42:33 +0900

    v4.0 kiosk try catch

--- week13_kiosk.py
+++ week13_kiosk.py
@@ -3,9 +3,18 @@ import kiosk as kk
 import requests
 
 if __name__ == "__main__":
-    url = f"https://wttr.in/suwon?format=%C+%t&lang=ko"
-    response = requests.get(url)
-    print(response.text.strip())# 습관적으로 .strip() 하는게 좋음
+    # url = f"https://wttr.in/suwon?format=%C+%t&lang=ko" #
+    # url = f"https://naver.com/kim" # 연결은 됨
+    url = f"https://wttr123.in/suwon?format=%C+%t&lang=ko"  # 페이지가 없음
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(response.text.strip())
+        else:
+            print(f"상태 코드 : {response.status_code}")
+    except Exception as err:
+        print(f"오류 : {err}")
+    #print(response.text.strip())# 습관적으로 .strip() 하는게 좋음
     kk.run()
     kk.print_receipt()
     kk.print_ticket_number()
